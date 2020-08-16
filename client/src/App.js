a046575795c2bfa982bd8ccacb80857bae1f615a
sidhi-sid <sidhisidh469@gmail.com>
2020-08-17 00:56:28 +0530

    starting with material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import './App.css';
 import Button from './component/button';
+import Header from './component/header';
+import { Grid , Typography} from '@material-ui/core';
 
+import PictureCard from './component/pictureCard';
 function App() {
   return (
     <div>
-    <Button/>
-    </div>
-      
+    <Header/>
+    <Grid style={{ marginLeft:25, marginRight:25 }}>
+        <Grid container style={{ marginTop:40 }}>
+        <Grid item xs={12} sm={6} md={6}>
+        <Typography style={styles.title}  variant={"h2"}>
+          You don't need to go far to find what matters.
+        </Typography>
+        </Grid>
+        </Grid>
+    <Grid container spacing={2} style={{marginTop:20}}>
+      <Grid item sm={3}>
+        <PictureCard title={"Nearby Getaways"} description={"qwerty"}/>
+      </Grid>
+      <Grid item sm={3}>
+        <PictureCard title={"Online Experience"}/>
+      </Grid>
+      <Grid item sm={3}>
+        <PictureCard title={"Entire Homes"}/>
+      </Grid>
+      <Grid item sm={3}>
+        <PictureCard/>
+      </Grid>
+    </Grid>
+    </Grid>
+    </div>   
   );
-}
+};
+
+const styles={
+  title: {
+    color: "#3f51b5",
+},
+};
+
+
+
+
 
 export default App;
---
+++ client/src/component/pictureCard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 240,
+  },
+});
+
+export default function PictureCard(props) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="https://picsum.photos/200/300"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+const propTypes= {
+    title:PropTypes.String.isRequired
+};
+    const defaultProps={
+        title:"Edit Title",
+    };
+
+  PictureCard.defaultProps=defaultProps;
+  PictureCard.propTypes=propTypes;
