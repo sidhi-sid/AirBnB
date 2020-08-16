bf4c76f8eb9a3b1721b02c9cc505d5a15929e569
sidhi-sid <sidhisidh469@gmail.com>
2020-08-16 20:57:36 +0530

    started with react

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Button from './component/button';
+
+function App() {
+  return (
+    <div>
+    <Button/>
+    </div>
+      
+  );
+}
+
+export default App;
---
+++ client/src/component/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button=()=>{
+   return(
+       <div>
+           <h1>My button</h1>
+           <button>Click</button>
+       </div>
+   ) ;
+};
+
+export default Button;
